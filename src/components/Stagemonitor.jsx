acdb0fbaf09a0ec864b1a77aca13cc234054a9c4
designeminentav-web <design.eminentav@gmail.com>
2025-12-19 15:50:16 +0530

    changDS

--- src/components/Stagemonitor.jsx
+++ src/components/Stagemonitor.jsx
@@ -9,11 +9,11 @@ import Footer from "../components/Footer";
 import { useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet";
 //newimg//
-import dsf5 from "../assets/ds5/dsf5.jpg";
-import dsf12 from "../assets/ds12/dsf12.jpg";
-import dsf15 from "../assets/ds15/dsf15.jpg";
+import dsf5 from "../assets/ds5/dsf5.JPG";
+import dsf12 from "../assets/ds12/dsf12.JPG";
+import dsf15 from "../assets/ds15/dsf15.JPG";
 import Hwfornt from "../assets/Hwfront.png";
-import dsf8 from "../assets/ds8/dsf8.jpg";
+import dsf8 from "../assets/ds8/dsf8.JPG";
 
 
 const products = [
