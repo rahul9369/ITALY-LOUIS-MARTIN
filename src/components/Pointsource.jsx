1ef6d9189747bd496c20e1e01d166552ee2aae7b
designeminentav-web <design.eminentav@gmail.com>
2025-12-19 16:18:57 +0530

    changPOINT

--- src/components/Pointsource.jsx
+++ src/components/Pointsource.jsx
@@ -10,7 +10,7 @@ import redf5 from "../assets/red6/redf5.JPG";
 import redff9 from "../assets/red9/redff9.JPG";
 import red412 from "../assets/red12/red412.jpg";
 import ffront8 from "../assets/f8/ffront8.jpg";
-import fopen10 from "../assets/f10/fopen10.jpg";
+import fopen10 from "../assets/f10/fopen10.JPG";
 import ff12 from "../assets/f12/ff12.jpg";
 import ff15 from "../assets/f15/ff15.jpg";
 
